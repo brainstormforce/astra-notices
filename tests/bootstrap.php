125ea7f4b6d53a842ae1b2b56f814a9abcd63417
Nikhil <email@nikhilchavan.com>
2022-02-08 21:44:27 +0530

    fix phpunit tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,27 +5,22 @@
  * @package Astra_Notices
  */
 
-$_tests_dir = getenv( 'WP_TESTS_DIR' );
-
-if ( ! $_tests_dir ) {
-	$_tests_dir = rtrim( sys_get_temp_dir(), '/\\' ) . '/wordpress-tests-lib';
-}
-
-if ( ! file_exists( $_tests_dir . '/includes/functions.php' ) ) {
-	echo "Could not find $_tests_dir/includes/functions.php, have you run bin/install-wp-tests.sh ?" . PHP_EOL; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
-	exit( 1 );
-}
-
-// Give access to tests_add_filter() function.
-require_once $_tests_dir . '/includes/functions.php';
+// Composer autoloader must be loaded before WP_PHPUNIT__DIR will be available
+require_once dirname( __DIR__ ) . '/vendor/autoload.php';
 
 /**
- * Manually load the plugin being tested.
- */
-function _manually_load_plugin() {
-	require dirname( dirname( __FILE__ ) ) . '/class-astra-notices.php';
-}
-tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
+  * Load PHPUnit Polyfills for the WP testing suite.
+  *
+  * @see https://github.com/WordPress/wordpress-develop/pull/1563/
+  */
+define( 'WP_TESTS_PHPUNIT_POLYFILLS_PATH', dirname( __DIR__, 2 ) . '/vendor/yoast/phpunit-polyfills/phpunitpolyfills-autoload.php' );
+
+// Give access to tests_add_filter() function.
+require_once getenv( 'WP_PHPUNIT__DIR' ) . '/includes/functions.php';
+
+tests_add_filter( 'muplugins_loaded', function() {
+    require dirname( dirname( __FILE__ ) ) . '/class-astra-notices.php';
+} );
 
 // Start up the WP testing environment.
-require $_tests_dir . '/includes/bootstrap.php';
+require getenv( 'WP_PHPUNIT__DIR' ) . '/includes/bootstrap.php';
