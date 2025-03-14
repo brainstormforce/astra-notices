010b2576ae9ed7aaeb8bffc563bb777896c60c66
Abhishek Bhikule <abhishekbhikule76@gmail.com>
2025-03-14 17:16:37 +0530

    corrected the if condition for astra-notice-id

--- class-astra-notices.php
+++ class-astra-notices.php
@@ -138,7 +138,7 @@ if ( ! class_exists( 'Astra_Notices' ) ) :
 			);
 
 			// if $notice_id does not start with astra-notices-id and notice_id is not from the allowed notices, then return.
-			if ( strpos( $notice_id, 'astra-notices-id-' ) === false && array_search( $notice_id, $allowed_notices ) === false ) {
+			if ( strpos( $notice_id, 'astra-notices-id-' ) !== 0 && array_search( $notice_id, $allowed_notices ) === false ) {
 				return;
 			}
 
@@ -235,7 +235,7 @@ if ( ! class_exists( 'Astra_Notices' ) ) :
 				)
 			);
 
-			return ! empty( $notice ) ? $notice[0] : array();
+			return ( ! empty( $notice ) && isset( $notice[0] ) ) ? $notice[0] : array();
 		}
 
 		/**
