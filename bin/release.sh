2664b1ce7979ddbefb191b19bb1e77ecac459101
Raj Mohan <68589327+rajmohanreddy774@users.noreply.github.com>
2026-04-13 20:42:28 +0530

    chore: point release.sh at renamed class-bsf-admin-notices.php

--- bin/release.sh
+++ bin/release.sh
@@ -18,7 +18,7 @@ if [[ -d "$BUILD_DIR" ]]; then
 fi
 
 COMMIT=$(git rev-parse HEAD)
-VERSION=$(grep 'private static $version = ' class-astra-notices.php | grep -oEi "'([0-9\.a-z\+-]+)';$" | sed "s/'//g; s/;//")
+VERSION=$(grep 'private static $version = ' class-bsf-admin-notices.php | grep -oEi "'([0-9\.a-z\+-]+)';$" | sed "s/'//g; s/;//")
 
 if [[ $VERSION != "null" ]]; then
     DEPLOY_BRANCH="release/${VERSION}"
