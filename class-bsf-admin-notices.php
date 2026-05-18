3d73f34fde0dfb78542bf5f81687a0da3fb2f62e
Raj Mohan M <68589327+rajmohanreddy774@users.noreply.github.com>
2026-05-19 04:01:04 +0530

    fix: bump version to 1.2.2

--- class-bsf-admin-notices.php
+++ class-bsf-admin-notices.php
@@ -33,7 +33,7 @@ if ( ! class_exists( 'BSF_Admin_Notices' ) ) :
 		 * @var string
 		 * @since 1.2.0
 		 */
-		private static $version = '1.2.1';
+		private static $version = '1.2.2';
 
 		/**
 		 * Registered notices.
